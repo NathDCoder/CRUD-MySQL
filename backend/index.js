c33cd9eb7e58546b6d304e18d648c2e01694afa4
NathDCoder <themusicemperor@gmail.com>
2023-02-20 11:17:44 -0500

    crudmysql

--- backend/index.js
+++ backend/index.js
@@ -11,10 +11,35 @@ const db = mysql.createConnection({
     database: "test"
 })
 
+//If there is an Auth error
+//ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Root123';
+
+
 app.get("/", (req, res) => {
     res.json("You Made it Traveller to the BE")
 })
 
+
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM books"
+    db.query(q, (err, data) => {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+
+
+
+app.post("/books", (req, res) =>{
+    const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)"
+    const values = ["title from backend", "desc from backend", "cover pic from backend"]
+
+    db.query(q, [values], (err, data) => {
+        if(err) return res.json(err)
+        return res.json("Book successfully created")
+    })
+})
 
 app.listen(8800, () => {
     console.log("connected to backend!1")
